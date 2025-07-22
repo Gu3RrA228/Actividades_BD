f210d539d48966a3deac71d5b56ac06aa8bc6382
Gu3RrA228 <pedroyahir2012@hotmail.com>
2025-07-22 01:26:50 -0600

    Add files via upload

---
+++ Base de datos.sql
@@ -0,0 +1,8 @@
+create table ALUMNOS(
+Pk_IdAlumno Number(10) Primary Key Not null,
+Nombre varchar(50),
+ApellidoPaterno varchar(50),
+ApellidoMaterno varchar(50),
+Edad Number(3),
+FechaDeNacimiento Date
+);
